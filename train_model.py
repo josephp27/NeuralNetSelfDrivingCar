1d63f6c9e2d7e54a4e1c21ce99387a62cba492d4
abelsaug <asa.11ec05@nitk.edu.in>
2018-05-05 20:38:08 -0500

    Train model changes

--- train_model.py
+++ train_model.py
@@ -19,24 +19,27 @@ session = tf.Session(config=config)
 
 model = alexnet(WIDTH, HEIGHT, LR, output = 4)
 
-for i in range(1,n_btch+1):
-    train_data = np.load('data/training_data-{}.npy'.format(i))
-    print('LOADED: training_data-{}.npy'.format(i))
-    
-    train = train_data[:-100]
-    test = train_data[-100:]
-    X = np.array([i[0] for i in train]).reshape(-1,WIDTH,HEIGHT,1)
-    Y = [i[1] for i in train]
+for epoch in range(EPOCHS):    
+    for i in range(1,n_btch+1):
+        train_data = np.load('data/training_data-{}.npy'.format(i))
+        print('LOADED: training_data-{}.npy'.format(i))
+        
+        train = train_data[:-100]
+        test = train_data[-100:]
+        X = np.array([i[0] for i in train]).reshape(-1,WIDTH,HEIGHT,1)
+        Y = [i[1] for i in train]
 
-    test_x = np.array([i[0] for i in test]).reshape(-1,WIDTH,HEIGHT,1)
-    test_y = [i[1] for i in test]
+        test_x = np.array([i[0] for i in test]).reshape(-1,WIDTH,HEIGHT,1)
+        test_y = [i[1] for i in test]
+
+        model.fit({'input': X}, {'targets': Y}, n_epoch=1, validation_set=({'input': test_x}, {'targets': test_y}), 
+            snapshot_step=500, show_metric=True,  batch_size=512, run_id=MODEL_NAME)
 
     os.chdir('weights')
-    model.fit({'input': X}, {'targets': Y}, n_epoch=EPOCHS, validation_set=({'input': test_x}, {'targets': test_y}), 
-        snapshot_step=500, show_metric=True,  batch_size=512, run_id=MODEL_NAME)
-
     model.save(MODEL_NAME)
     os.chdir('..')
+    
+
 
     
 
