6796e19c47e7c47a60c61e24bcff72eda6284cb2
Joseph <josephp27@live.com>
2018-05-06 15:57:37 -0500

    updating balancer

--- balance_data.py
+++ balance_data.py
@@ -1,43 +1,51 @@
-# balance_data.py
-
 import numpy as np
 import pandas as pd
 from collections import Counter
 from random import shuffle
+import os
 
-n_btch = 71
-for i in range (1,n_btch+1):
+def num_Batch():
+	path, dirs, files = next(os.walk(os.getcwd() + '/data/'))
+	return len(files)
+
+print(num_Batch())
+for i in range (1,num_Batch() + 1):
     train_data = np.load('data/training_data-{}.npy'.format(i))
 
     df = pd.DataFrame(train_data)
-    print(df.head())
-    print(Counter(df[1].apply(str)))
+    # print(df.head())
+    # print(Counter(df[1].apply(str)))
 
     lefts = []
     rights = []
     forwards = []
     backwards = []
-    
-    shuffle(train_data)
+    no_op = []
 
     for data in train_data:
         img = data[0]
         choice = data[1]
 
-        if choice[2] == 1:
+        if choice[1] == 1:
+            backwards.append([img,choice])
+        elif choice[2] == 1:
             lefts.append([img,choice])
         elif choice[3] == 1:
             rights.append([img,choice])
-        elif choice[1] == 1:
-            backwards.append([img,choice])
         elif choice[0] == 1:
             forwards.append([img,choice])
         else:
-            print('no matches')
+            no_op.append([img,choice])
 
-    forwards = forwards[:max(len(lefts), len(rights), len(backwards))]
+    smallest_class = min(len(lefts), len(rights), len(backwards), len(forwards))
+    
+    forwards = forwards[:smallest_class]
+    lefts = lefts[:smallest_class]
+    rights = rights[:smallest_class]
+    backwards = backwards[:smallest_class]
+    no_op = no_op[:smallest_class]
 
-    final_data = forwards + lefts + rights + backwards
-    shuffle(final_data)
+    final_data = forwards + lefts + rights + backwards + no_op
 
+    print(len(final_data))
     np.save('data/training_data-{}.npy'.format(i), final_data)
