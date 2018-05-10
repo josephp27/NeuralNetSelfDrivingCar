195a092da252f3c935423797ed965927e2b3b937
abelsaug <asa.11ec05@nitk.edu.in>
2018-05-10 18:13:58 -0500

    Modified balanced 4

--- balance_data_4.py
+++ balance_data_4.py
@@ -3,7 +3,6 @@ import pandas as pd
 from collections import Counter
 from random import shuffle
 import os
-import h5py
 
 lefts = []
 rights = []
@@ -46,13 +45,23 @@ for filename in os.listdir(os.getcwd() + '/data/'):
             # backwards_right.append([img, choice])
             backwards.append([img,choice])            
 
-        elif choice == [0, 1, 1, 0]:       	
+        elif choice == [0, 1, 1, 0]:        
             # backwards_left.append([img, choice])
             backwards.append([img,choice])
 
-        final_data = forwards + lefts + rights + backwards + no_op
-   
-        np.savez('data/training_data-merged-{}.npz'.format(num), final_data)
+smallest_class = min(len(forwards), len(rights), len(lefts), len(backwards), len(no_op))
 
-        num += 1
+lefts = lefts[:smallest_class]
+rights = rights[:smallest_class]
+forwards = forwards[:smallest_class]
+backwards = backwards[:smallest_class]
+no_op = no_op[:smallest_class]
 
+print('Lefts {} Rights {} Backwards {} Forwards {} NOOP {}'.format(len(lefts), 
+    len(rights), len(backwards), len(forwards), len(no_op) ))
+
+final_data = lefts + left_forwards + rights + right_forwards + forwards + backwards + backwards_left + backwards_right + no_op
+
+chunks = np.array_split(final_data, 5)
+for i in range(len(chunks)):        
+    np.save('data/training_data-balanced-{}.npy'.format(i), chunks[i])
